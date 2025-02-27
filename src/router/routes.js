488f2b3a5e91fa0d73b3f15a6e89bc30f354b9bb
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2025-02-27 20:37:47 +0500

    upd

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,7 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/LandingLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
-      { path: "test", component: () => import("pages/IndexPage_copy.vue") },
-    ],
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
 
   // Always leave this as last one,
