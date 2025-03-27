4d7ded8df5387fddb93af0cb956fa2a94ec5f003
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2025-03-27 15:26:52 +0500

    upd

--- src/components/AppHeaderCompact.vue
+++ src/components/AppHeaderCompact.vue
@@ -2,7 +2,8 @@
     <div class="fake_header_wrapper flex justify-center q-py-md q-mb-md" style="width: 100%; max-height: 10vh;">
         <div class="fake_header row flex items-center" style="width: 100%; height: 80px;"
             :class="$q.screen.gt.sm ? 'justify-between q-px-xl' : 'justify-between q-px-md'">
-            <div style="min-width: 150px;max-width: 150px;">
+            <div
+                :style="$q.screen.gt.xs ? 'min-width: 150px; max-width: 150px;' : 'min-width: 110px; max-width: 110px;'">
                 <q-img src="src/assets/logo.svg" style="height: 100%; width: 100%;" />
             </div>
             <div class="row" style="min-width: 150px;max-width: 550px;"
@@ -20,19 +21,32 @@
                     class="text-primary text-center q-mx-md h6">Партнеры
                 </AppLink_NewTabIfExt>
             </div>
-            <div class="q-gutter-md row">
-                <div v-if="$q.screen.gt.sm">
-                    <q-btn v-if="!isUserLoggedIn" no-caps flat color="green" size="lg" text-color="primary"
+            <div class="header_buttons">
+
+                <div v-if="showLandingLinks == true" class="q-gutter-md row justify-center">
+                    <q-btn v-if="isUserLoggedIn" no-caps flat color="green" size="lg" text-color="primary"
                         style="background-color: #EFDFBB;border: solid 2px #315720;" @click="showAuthDialog = true">
                         Войти
                     </q-btn>
-                    <q-btn v-else no-caps flat color="green" size="lg" text-color="primary"
+                    <q-btn v-if="!isUserLoggedIn" no-caps flat color="green" size="lg" text-color="primary"
+                        style="background-color: #EFDFBB;border: solid 2px #315720;" @click="showAuthDialog = true"
+                        icon="img:src/assets/images/icons/logout.svg">
+                        Выйти
+                    </q-btn>
+                    <q-btn v-if="$q.screen.lt.lg" flat no-caps color="green" icon="menu"
+                        @click="showMobileDialog = true" size="lg" />
+                </div>
+                <div v-if="showLandingLinks == false">
+                    <q-btn v-if="isUserLoggedIn" no-caps flat color="green" size="lg" text-color="primary"
                         style="background-color: #EFDFBB;border: solid 2px #315720;" @click="showAuthDialog = true">
                         Войти
+                    </q-btn>
+                    <q-btn v-if="!isUserLoggedIn" no-caps flat color="green" size="lg" text-color="primary"
+                        style="background-color: #EFDFBB;border: solid 2px #315720;" @click="showAuthDialog = true"
+                        icon="img:src/assets/images/icons/logout.svg">
+                        Выйти
                     </q-btn>
                 </div>
-                <q-btn v-if="$q.screen.lt.lg" flat no-caps color="green" icon="menu" @click="showMobileDialog = true"
-                    size="lg" />
 
             </div>
         </div>
